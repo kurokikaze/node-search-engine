c3f6aec097542b5b880775c143e4442c200df17f
Kuroki Kaze <kurokikaze@yandex.ru>
2010-03-24 13:27:55 +0300

    Parsing Habrahabr homepage with Parser

--- spider.js
+++ spider.js
@@ -1,24 +1,10 @@
 var libxml = require("./libxmljs"),
     sys = require("sys");
 
-var xml = '<date><year>2010</year><month>March</month><day>24</day></date>';
+var parsed = libxml.parseHtmlFile('habr.htm');
 
-var parser = new libxml.SaxParser(function(cb) {
-  cb.onStartDocument(function() {});
-  cb.onEndDocument(function() {});
-  cb.onStartElementNS(function(elem, attrs, prefix, uri, namespaces) {
-    sys.puts('Element: ' + elem);
-  });
-  cb.onEndElementNS(function(elem, prefix, uri) {});
-  cb.onCharacters(function(chars) {
-  //  sys.puts('Characters: ' + chars);
-
-  });
-  cb.onCdata(function(cdata) {});
-  cb.onComment(function(msg) {});
-  cb.onWarning(function(msg) {});
-  cb.onError(function(msg) {});
-});
-
-//parser.parseString(xml);
-parser.parseFile('google.htm');
+sys.puts(parsed.encoding());
+var links = parsed.find('//a');
+for (link in links) {
+    sys.puts('Destination: ' + links[link].attr('href').value());
+}
