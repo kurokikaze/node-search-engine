97b22f11fc1cdc11f8d0a4f88c2b992cc6c11f90
Kuroki Kaze <kurokikaze@yandex.ru>
2010-04-15 12:28:33 +0400

    Made spider save crawled pages in CouchDB

---
+++ settings.js
@@ -0,0 +1,7 @@
+process.mixin(exports, {
+
+    couchhost: '192.168.175.128:5984',
+    couchbase: 'spider',
+    targethost: 'finalfantasy.wikia.com'
+    
+});
--- spider.js
+++ spider.js
@@ -1,5 +1,3 @@
-var host_to_crawl = "wikipedia.org";
-
 var unique = function(arr) {
     var a = [];
     var l = arr.length;
@@ -15,28 +13,35 @@ var unique = function(arr) {
 };
 
 function indexInArray(arr, val){
-    for (var i = 0; i < arr.length; i++) { 
+    for (var i = 0; i < arr.length; i++) {
 	    if(arr[i]==val) return true;
     }
     return false;
 }
 
+function get_content_type(headers) {
+    return headers['content-type'].split(';')[0];
+}
+
 var libxml = require("./libxmljs"),
     http = require("http"),
     url = require("url"),
+    settings = require("./settings"),
+    couch = require("./node-couch").CouchDB,
     sys = require("sys");
 
-var target_site = http.createClient(80, host_to_crawl);
+var target_site = http.createClient(80, settings.targethost);
+var db = couch.db(settings.couchbase, settings.couchhost);
+
 
 var parsePage = function(string) {
-    try {	
+    try {
         var parsed = libxml.parseHtmlString(string);
     } catch(e) {
 	sys.puts('Cannot parse: ' + string);
 	return [];
     }
 
-//    sys.puts(parsed.encoding());
     var links = parsed.find('//a');
     var destinations = [];
     for (link in links) {
@@ -44,11 +49,12 @@ var parsePage = function(string) {
         if (attr && attr.value) {
             var url_parts = url.parse(attr.value());
 
-	    if (!url_parts.hostname || url_parts.hostname.indexOf(host_to_crawl) > -1) {
+	        if (!url_parts.hostname || url_parts.hostname.indexOf(settings.targethost) > -1) {
             	destinations.push(url_parts.pathname);
-	    } else {
-		sys.puts('Found outbound link to ' + url_parts.hostname);
-	    }
+	        } else {
+		        // sys.puts('Found outbound link to ' + url_parts.hostname);
+	        }
+
         }
     }
 
@@ -57,7 +63,7 @@ var parsePage = function(string) {
 
 var getPage = function(URL, callback) {
 
-    var request = target_site.request("GET", URL, {"host": host_to_crawl});
+    var request = target_site.request("GET", URL, {"host": settings.targethost});
 
     request.addListener('response', function (response) {
       response.setBodyEncoding("utf8");
@@ -69,11 +75,13 @@ var getPage = function(URL, callback) {
       });
 
       response.addListener('end', function() {
-          callback(text);
+//          sys.puts('URL: ' + URL + ' > ' + response.statusCode);
+//          sys.puts('HEADERS > ' + JSON.stringify(response.headers));
+          callback(response.statusCode, text, response.headers);
       });
 
     });
-    request.close();
+    request.end();
 };
 
 var known_pages = [];
@@ -82,22 +90,54 @@ var visited_pages = [];
 
 var get_next_page = function() {
     for (page in known_pages) {
-        if (known_pages[page] && !indexInArray(visited_pages, known_pages[page])) {
+        if (known_pages[page] && !indexInArray(visited_pages, known_pages[page]) && (typeof known_pages[page] != 'undefined')) {
             visited_pages.push(known_pages[page]);
+            // sys.puts(known_pages[page] + ' marked as visited');
             sys.puts('Visited pages: ' + visited_pages.length);
             return known_pages[page];
         }
     }
+
+    process.exit(); // End of list
 }
 
-var crawl_page = function(URL) {
+var crawl_page = function (URL) {
     sys.puts('Visiting ' + URL);
-    getPage(URL, function(text) {
-        var links = parsePage(text);
+    getPage(URL, function(code, text, headers) {
+
+        sys.puts('Got ' + code + ' answer, headers is: ' + JSON.stringify(headers));
+        var links = [];
+
+        if (code == 200) {
+            var content_type = get_content_type(headers);
+
+            if (content_type == 'text/html' || content_type == 'text/plain' || content_type == '') {
+                links = parsePage(text);
+                save_page(URL, text, code);
+            } else {
+                sys.puts('Strange content type: ' + content-type);
+            }
+
+        } else if (code == 301 || code == 303) {
+
+            // Return redirect location to known pages
+            links = [headers.location];
+
+        } else if (code == 404) {
+            // Do nothing, maybe add some sort of log entry
+        } else {
+            sys.puts('Unknown code: ' + code + '\nHeaders is: ' + JSON.stringify(headers));
+        }
+
+
         known_pages = unique(known_pages.concat(links));
         sys.puts('Known pages: ' + known_pages.length);
         crawl_page(get_next_page());
     });
 }
 
-crawl_page('/');
+var save_page = function (URL, text) {
+    db.saveDoc({'url' : URL, 'text' : text});
+}
+
+crawl_page('/wiki/User_talk:Crazyswordsman/archive_8');
