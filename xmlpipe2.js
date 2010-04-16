cf842786412aa2031688c29c76c82773370e6b23
Kuroki Kaze <kurokikaze@yandex.ru>
2010-04-16 18:05:57 +0400

    Added URL field to document, cleaned code a bit

--- xmlpipe2.js
+++ xmlpipe2.js
@@ -3,35 +3,9 @@ var couch = require('./node-couch').CouchDB,
     settings = require('./settings'),
     sys = require('sys');
 
-function Combo(callback) {
-  this.callback = callback;
-  this.items = 0;
-  this.results = [];
-}
-
-Combo.prototype = {
-  add: function () {
-    // sys.puts('Add ' + this.items);
-    var self = this;
-    this.items++;
-    return function () {
-      self.check(self.items - 1, arguments);
-    };
-  },
-  check: function (id, arguments_in) {
-    // sys.puts('Check ' + id);
-    this.results[id] = arguments_in;
-    this.items--;
-    if (this.items == 0) {
-      this.callback.call(this, this.results);
-    }
-  }
-};
-
-
 var db = couch.db(settings.couchbase, settings.couchhost);
 
-process_document = function (docs) {
+var process_document = function (docs) {
     if (docs.length > 0) {
         var document = docs.pop();
 
@@ -39,14 +13,29 @@ process_document = function (docs) {
             'success': function(page) {
                 sys.puts('<sphinx:document id="' + page._id + '">');
 
-                sys.puts('<subject><![CDATA[[');
-                sys.puts(page.title);
+                sys.puts('<subject><![CDATA[');
+
+                if (page.title) {
+                    sys.puts(page.title.replace(']]>', ''));
+                } else {
+                    sys.puts('No title');
+                }
+
                 sys.puts(']]></subject>');
 
-                sys.puts('<content><![CDATA[[');
-                sys.puts(page.text);
+                sys.puts('<content><![CDATA[');
+
+                if (page.text) {
+                    sys.puts(page.text.replace(']]>', ''));
+                }
+
                 sys.puts(']]></content>');
 
+                if (page.url) {
+                    sys.puts('<url>' + page.url + '</url>');
+                } else {
+                    sys.puts('<url />');
+                }
                 sys.puts('<published>' + (new Date()).getTime().toString() + '</published>');
 
                 sys.puts('</sphinx:document>');
@@ -65,47 +54,19 @@ process_document = function (docs) {
 
 db.allDocs({
     'success': function(docs) {
-        //sys.puts('Got all docs: ' + JSON.stringify(docs));
 
+        sys.puts('<' + '?xml version="1.0" encoding="utf-8"?>');
         sys.puts('<sphinx:docset>');
         sys.puts('<sphinx:schema>');
 
-        sys.puts('<sphinx:field name="subject" />');
-        sys.puts('<sphinx:field name="content" />');
-        sys.puts('<sphinx:field name="published" type="timestamp" />');
+        sys.puts(' <sphinx:field name="subject" />');
+        sys.puts(' <sphinx:field name="content" />');
+        sys.puts(' <sphinx:field name="url" />');
+        sys.puts(' <sphinx:field name="published" type="timestamp" />');
 
         sys.puts('</sphinx:schema>');
 
         process_document(docs.rows);
-        /*for (doc in docs.rows) {
-            if (docs.rows[doc]) {
-                //sys.puts('Fetch ' + docs.rows[i].id);
-
-            }
-        } */
-
-        /*var xmldoc = new libxml.Document(function(n) {
-          n.node('sphinx:docset', function(n) {
-            n.node('sphinx:schema', function(n) {
-              n.node('sphinx:field', {'name':'subject'});
-              n.node('sphinx:field', {'name':'content'});
-              n.node('sphinx:field', {'name':'published', 'type':'timestamp'});
-            });
-            for (doc in data) {
-
-                // sys.puts(JSON.stringify(data[doc]));
-
-                var doc_fields = data[doc]['0'];
-                n.node('sphinx:document', {'id':doc_fields._id}, function(n){
-                    n.node('subject', '<![CDATA[[' + doc_fields.title + ']]>');
-                    n.node('content', '<![CDATA[[' + doc_fields.text + ']]>');
-                    n.node('published', (new Date()).getTime().toString());
-                });
-            }
-          });
-        });
-
-        sys.write(xmldoc);*/
 
     },
     'error': function(errorResponse) {
