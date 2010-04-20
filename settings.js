1abd4848e4f154447933b86a91c5040529393d97
Kuroki Kaze <kurokikaze@yandex.ru>
2010-04-20 12:58:12 +0400

    Added crawl timeout to settings

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
-process.mixin(exports, {
-
-    couchhost: '192.168.175.128:5984',
-    couchbase: 'spider',
-    targethost: 'finalfantasy.wikia.com',
-    max_streams: 3
-
-});
+exports.couchhost = '192.168.175.128:5984';
+exports.couchbase = 'spider';
+exports.targethost = 'finalfantasy.wikia.com';
+exports.max_streams = 3;
+exports.max_streams = 3;
+exports.crawl_timeout = 500;
